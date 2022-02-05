953927dfb89e95d4ad8463ef9f661240f8a6e54b
Андрей щербаков <sherbakov12342@gmail.com>
2022-02-05 18:58:35 +0300

    ZALUPA

--- Stack/Source.cpp
+++ Stack/Source.cpp
@@ -26,11 +26,14 @@ void push(struct stack* stk, float f) {
 	if (stk->top < MAX_SIZE) {
 		stk->elem[stk->top] = f;
 		stk->top++;
+		cout << f << " ";
 	}
 	else
 		cout << "The stack is full, the number of elements: " << stk->top << " ";
 
 }
+
+
 int isempty(struct stack* stk) {
 	if (stk->top == 0)    return 1;
 	else return 0;
@@ -51,43 +54,65 @@ float pop(struct stack* stk) {
 	}
 }
 
+void filter(struct stack* stk) {
+	int i;
+	i = stk->top; // i - number of elements in the stack
+	do {
+		i--;
+		if (stk->elem[i] >= -50 && stk->elem[i] <= 50) {
+			stk->elem[i];
+			cout << stk->elem[i] << " ";
+		}
+	} while (i > 0);
+
+}
+
+
 void stkPrint(struct stack* stk) {
 	int i;
 	i = stk->top; // i - number of elements in the stack
 	if (isempty(stk) == 1) return; // stack is empty
-	cout << "Positive numbers in this stack: ";
+	cout << endl;
 	do {
 		i--;
-		if (stk->elem[i] > 0) {
-			cout << stk->elem[i] << " ";
-		}
+		cout << stk->elem[i] << " ";
 
 	} while (i > 0);
 
 	cout << endl;
 }
+
+
+
 int main() {
 	setlocale(LC_ALL, "ru");
 
 	struct stack* stk;
 
-	const int max = 50, min = -50;
+	const int max = MAX_SIZE, min = -MAX_SIZE;
 	stk = (struct stack*)malloc(sizeof(struct stack));
-	init(stk);
+	init(stk); // инициализация стека init(stk), где s — стек
 
-	for (int i = min; i < max; i++) {
-		cout << i << " ";
-		push(stk, i);
+	cout << "---------------------------------" << endl;
+	cout << "Вывод всех чисел: " << endl;
+	for (int i = 0; i < MAX_SIZE; i++) {
+		push(stk, min + rand() % ((max + 1) - min)); //помещение элемента в стек push(stk, i), где stk — стек, i — помещаемый элемент;
 	}
 	cout << endl;
 	cout << "In the stack " << getcount(stk) << " items" << endl;
-	stkPrint(stk);
+	cout << "---------------------------------" << endl;
+	stkPrint(stk); // вывод элементов стека stkPrint(stk), где s — стек
 	cout << endl;
+	cout << "FILTER" << endl;
+	filter(stk); // ВОТ ЭТА ХУЕТА ПО ИДЕЕ ДОЛЖНА ФИЛЬТРОВАТЬ ТУ ХУЕТУ
+	cout << endl;
+	stkPrint(stk); // НУ А ЭТА ХУЕТА ВЫВОДИТ
+	cout << "FILTER END" << endl;
 	cout << "Removing the stacks..." << endl;
 	do {
-		pop(stk); // Removing elements
-		getcount(stk);  // We get the number of elements
+		cout << "Извлекаем элемент: " << pop(stk) << endl; // Removing elements. удаление элемента из стека i=pop(stk);
 	} while (isempty(stk) == 0);
+	cout << endl;
 	cout << "In the stack " << getcount(stk) << " items" << endl;
 
 	return 0;
