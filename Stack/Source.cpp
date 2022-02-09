568c7637e9f0f51ffcf523fb126cd8ddb6c4f84f
Андрей щербаков <sherbakov12342@gmail.com>
2022-02-09 13:49:41 +0300

    FINAL

--- Stack/Source.cpp
+++ Stack/Source.cpp
@@ -47,14 +47,25 @@ tstk* ReadStackD(tstk* sp, int& inf)
 }
 
 void Printst(tstk* sp) {
-    while (sp)
-    {
+    while (sp) {
         cout << sp->inf << " ";
         sp = sp->a;
     }
     cout << endl;
 }
 
+tstk* DelStackAll(tstk* sp)
+{
+    tstk* spt; int inf;
+    while (sp != NULL) {
+        spt = sp;
+        inf = sp->inf;
+        cout << inf << " ";
+        sp = sp->a;
+        delete spt;
+    }
+    return NULL;
+}
 
 void main()
 {
@@ -62,23 +73,37 @@ void main()
 
     tstk* sp = NULL;
     const int max = 50, min = -50;
-    for (int i = 0; i < MAX_SIZE + 1; i++) {
-        //cout << min + rand() % ((max + 1) - min) << " ";
-        if ((min + rand() % ((max + 1) - min)) > 0) {
-            sp = add(sp, min + rand() % ((max + 1) - min));
-        }
+
+    cout << "1 Стек: " << endl;
+    
+    for (int i = 0; i < 110; i++) {
+        int randNumber = min + rand() % ((max + 1) - min);
+        sp = add(sp, randNumber);
     }
-    cout << endl << endl;
-    cout << "Размер: " << sp->size << endl << endl;
 
     Printst(sp);
-    //tstk* sp2 = NULL;
-    //while (sp)
-    //{
-    //    int inf;
-    //    sp = ReadStackD(sp, inf);
-    //    if (inf > 0) sp2 = add(sp2, inf);
-    //}
+    cout << endl;
+    cout << "Размер 1-го стека: " << sp->size << endl;
+    cout << endl;
+
+    cout << "2 Стек содержащий только положительные числа из первого стека: " << endl;
+    tstk* sp2 = NULL;
+    while (sp)
+    {
+        int inf;
+        sp = ReadStackD(sp, inf);
+        if (inf % 2 == 0) sp2 = add(sp2, inf);
+    }
+
+    Printst(sp2);
+    cout << endl;
+    cout << "Размер 2-го стека: " << sp2->size << endl;
+    cout << endl;
+
+    cout << "во втором стеке были удалены такие элементы: " << endl;
+    sp2 = DelStackAll(sp2);
+
+    cout << endl;
 }
 
 
