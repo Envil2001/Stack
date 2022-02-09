ce4f4a3412e8c77826dc6ce5796a005f46ac5872
Андрей щербаков <sherbakov12342@gmail.com>
2022-02-09 13:50:05 +0300

    FINAL

--- Stack/Source.cpp
+++ Stack/Source.cpp
@@ -76,7 +76,7 @@ void main()
 
     cout << "1 Стек: " << endl;
     
-    for (int i = 0; i < 110; i++) {
+    for (int i = 0; i < 100; i++) {
         int randNumber = min + rand() % ((max + 1) - min);
         sp = add(sp, randNumber);
     }
